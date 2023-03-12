117242c310880d0494380ca2164ce44d5cebf1ba
Satish Surath <s@n-m.co>
2023-03-12 17:53:50 -0400

    Initial Upload for the Templates Code
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Check back soon... and Hello, World!"
+    user = {'username': 'Test User'}
+    return render_template('index.html', title='Home', user=user)
