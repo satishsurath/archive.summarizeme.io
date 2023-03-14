a00c56fee32c2d6b787e6aaa5003ef5346c455a5
Satish Surath <s@n-m.co>
2023-03-13 21:40:16 -0400

    Adding Summarize URL placeholder
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/forms.py
+++ app/forms.py
@@ -4,12 +4,11 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.widgets import TextArea
 from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
 
-class Summarize4Me(FlaskForm):
+class SummarizeFromText(FlaskForm):
+    summarize = StringField('Paste / type the text to summarize below:', widget=TextArea())
+    submit = SubmitField('Summarize')
+
+class SummarizeFromURL(FlaskForm):
     summarize = StringField('Paste / type the text to summarize below:', widget=TextArea())
     submit = SubmitField('Summarize')
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,13 @@
 from flask import render_template
 from app import app
-from app.forms import LoginForm, Summarize4Me
+from app.forms import SummarizeFromText, SummarizeFromURL
 from flask import render_template, flash, redirect, url_for
 
 import json
 
 import os
 import openai
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 openAI_summary = "" 
@@ -18,29 +19,36 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
-
-@app.route('/summarize', methods=['GET', 'POST'])
-def summarize():
-    form = Summarize4Me()
+@app.route('/summarizeText', methods=['GET', 'POST'])
+def summarizeText():
+    form = SummarizeFromText()
     global openAI_summary
     global test2summarize
     if form.validate_on_submit():
         flash('Summary requested for:{}...'.format(form.summarize.data)[0:100])
         openAI_summary = openAI_summarize(form.summarize.data)
         test2summarize = form.summarize.data
-        return redirect(url_for('summarize'))
+        return redirect(url_for('summarizeText'))
     if (openAI_summary):
-      return render_template('summarize.html', title='Summarize', form=form,test2summarize=test2summarize, openAI_summary=openAI_summary)
+      return render_template('summarizeText.html', title='Summarize From Text', form=form,test2summarize=test2summarize, openAI_summary=openAI_summary)
     else:
-        return render_template('summarize.html', title='Summarize', form=form)
+        return render_template('summarizeText.html', title='Summarize From Text', form=form)
+
+@app.route('/summarizeURL', methods=['GET', 'POST'])
+def summarizeURL():
+    form = SummarizeFromURL()
+    global openAI_summary
+    global test2summarize
+    if form.validate_on_submit():
+        flash('Summary requested for:{}...'.format(form.summarize.data)[0:100])
+        openAI_summary = openAI_summarize(form.summarize.data)
+        test2summarize = form.summarize.data
+        return redirect(url_for('summarizeURL'))
+    if (openAI_summary):
+      return render_template('summarizeURL.html', title='Summarize From URL', form=form,test2summarize=test2summarize, openAI_summary=openAI_summary)
+    else:
+        return render_template('summarizeURL.html', title='Summarize From URL', form=form)
+
 
 def openAI_summarize(form_prompt):
     response = openai.Completion.create(
