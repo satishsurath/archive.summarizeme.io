ab1b70806ded440076300637bf04e7d82fbe501a
Satish Surath <s@n-m.co>
2023-03-14 22:15:55 -0400

    Adding Cloudflare Turnstile feature along with flask_turnstile
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_turnstile import Turnstile
+
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+turnstile = Turnstile(app=app)
+
 
 
 from app import routes
--- config.py
+++ config.py
@@ -4,3 +4,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #These are needed for Cloudflare TURNSTILE: https://developers.cloudflare.com/turnstile/
+    TURNSTILE_SITE_KEY = os.environ.get('TURNSTILE_SITE_KEY') or "3x00000000000000000000FF"
+    TURNSTILE_SITE_SECRET = os.environ.get('TURNSTILE_SITE_SECRET') or "2x0000000000000000000000000000000AA"
+    TURNSTILE_ENABLED = True
