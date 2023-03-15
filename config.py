2b49c8f990407a18f29d98a90e59bb179b21af56
Satish Surath <s@n-m.co>
2023-03-14 22:36:06 -0400

    chaging the default Cloudflare TURNSTILE_SITE_KEY and  TURNSTILE_SITE_SECRET values that 'always allow'
    Signed-off-by: Satish Surath <s@n-m.co>

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     #These are needed for Cloudflare TURNSTILE: https://developers.cloudflare.com/turnstile/
-    TURNSTILE_SITE_KEY = os.environ.get('TURNSTILE_SITE_KEY') or "3x00000000000000000000FF"
-    TURNSTILE_SITE_SECRET = os.environ.get('TURNSTILE_SITE_SECRET') or "2x0000000000000000000000000000000AA"
+    #The Default values are for testing and will "Always Allow": https://developers.cloudflare.com/turnstile/reference/testing/
+    TURNSTILE_SITE_KEY = os.environ.get('TURNSTILE_SITE_KEY') or "1x00000000000000000000AA"
+    TURNSTILE_SITE_SECRET = os.environ.get('TURNSTILE_SITE_SECRET') or "1x0000000000000000000000000000000AA"
     TURNSTILE_ENABLED = True
