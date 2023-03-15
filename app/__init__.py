d71038e03b9cde97987d4154bfb97f02e20d87e6
Satish Surath <s@n-m.co>
2023-03-15 12:01:55 -0400

    Simplifying the code and removed all traces of Flask-Turnstile
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from flask_turnstile import Turnstile
+
 
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
-turnstile = Turnstile(app=app)
+
 
 
 
