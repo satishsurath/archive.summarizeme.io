860c922c17d652a668d5bd3a62e29d86646366d9
Satish Surath <s@n-m.co>
2023-03-16 23:45:44 -0400

    Adding support for storing the entries to a local sqlite file
    Signed-off-by: Satish Surath <s@n-m.co>

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from app import db
+from datetime import datetime
+
+class Entry_Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime,index=True, default=datetime.utcnow)
+    posttype = db.Column(db.Integer, index=True) # 0 means Summarize from Text, 1 means from URL...
+    url = db.Column(db.String(2048), index=True) # "0" for Summarize from Text; else URL string
+    test2summarize = db.Column(db.String(214748364), index=True)
+    openAIsummary = db.Column(db.String(214748), index=True)
+    def __repr__(self):
+        return '<Entry_Posts {}>'.format(self.id)
