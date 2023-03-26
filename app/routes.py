c5a700678d1ca90aaad6fd30cbbe008bccb1e962
Satish Surath <s@n-m.co>
2023-03-26 08:52:30 -0400

    Changing to gpt-3.5-turbo model instead of text-davinci-003
    Signed-off-by: Satish Surath <s@n-m.co>

--- app/routes.py
+++ app/routes.py
@@ -137,20 +137,17 @@ def summarizeURL():
 
 
 def openAI_summarize(form_prompt):
-    response = openai.Completion.create(
-      model="text-davinci-003",
-      prompt="Summarize the below text in a few short bullet points: \n\n"+form_prompt,
+    response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[{"role": "user", "content": "Summarize the below text in a few short bullet points: \n\n"+form_prompt}],
       temperature=0.7,
       max_tokens=100,
       top_p=1.0,
       frequency_penalty=0.0,
       presence_penalty=1
     )
-    print("\n",form_prompt)
-    print("\n",response)
-    print("\n",response["choices"][0]["text"][2:])
-    text_to_return = response["choices"][0]["text"][2:]
-    #text_to_return = text_to_return.split('\n')
+
+    text_to_return = response["choices"][0]['message']['content']
     print(text_to_return)
     return text_to_return
 
@@ -160,9 +157,3 @@ def write_to_db(posttype, url, test2summarizedb, openAIsummarydb):
   db.session.add(entry)
   db.session.commit()
   db.session.close()
-
-#    type = db.Column(db.Integer, index=True) # 0 means Summarize from Text, 1 means from URL...
-#    url = db.Column(db.String(2048), index=True) # "0" for Summarize from Text; else URL string
-#    test2summarize = db.Column(db.String(214748364), index=True)
-#    openAIsummary = db.Column(db.String(214748), index=True)
-    
