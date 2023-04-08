25115623bf2314a42acdfaae080c0933ccf2e577
Satish Surath <connect@sati.sh>
2023-04-08 07:07:49 +0530

    Addimg more verbose logging: Now includes Session Variable Data as well as User-Agent Information
    Signed-off-by: Satish Surath <connect@sati.sh>

--- app/__init__.py
+++ app/__init__.py
@@ -43,19 +43,32 @@ if not app.debug:
             toaddrs=app.config['ADMINS'], subject='SummarizeMe.io Failure',
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
+        email_handler.setFormatter(SessionDataFormatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]\n'
+            'Request data: %(request_data)s\n'
+            'Session data: %(session_data)s\n'
+            'User Agent: %(user_agent)s\n'
+        ))
         app.logger.addHandler(mail_handler)
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
-    file_handler = RotatingFileHandler(
-        'logs/summarizeme.io.log', 
-        maxBytes=10240, 
-        backupCount=10
-    )
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+file_handler = RotatingFileHandler(
+    'logs/summarizeme.log', 
+    maxBytes=10240, 
+    backupCount=10
+)
+file_handler.setFormatter(SessionDataFormatter(
+    '%(asctime)s %(levelname)s: %(message)s '
+    '[in %(pathname)s:%(lineno)d]\n'
+    'Request data: %(request_data)s\n'
+    'Session data: %(session_data)s\n'
+    'User Agent: %(user_agent)s\n'
+))
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('--------SummarizeMe startup-----------')
