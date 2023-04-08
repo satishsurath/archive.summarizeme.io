42b122c9b4d21444bca3194c446cf73007ad29bb
Satish Surath <connect@sati.sh>
2023-04-08 06:42:36 +0530

    Adding Logging support to local subdirectory as well as SMTP Email logging support
    Signed-off-by: Satish Surath <connect@sati.sh>

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
+import os
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_session import Session
-#from flask_login import login_user, logout_user, login_required, LoginManager, current_user
-
 from flask_login import LoginManager
 
 app = Flask(__name__)
@@ -15,6 +17,7 @@ migrate = Migrate(app, db)
 # Initialize the session
 Session(app)
 
+# Initialize the login manager
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
@@ -22,7 +25,45 @@ login_manager.init_app(app)
 from app import routes, models
 from app.models import Entry_Post
 
+# ---------------  Configure logging --------------- #
+# This is the logging configuration for the app
+# Check if the app is in debug mode
+if not app.debug:
+    # Set up email error logging
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='SummarizeMe.io Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler(
+        'logs/summarizeme.io.log', 
+        maxBytes=10240, 
+        backupCount=10
+    )
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('--------SummarizeMe startup-----------')
+
+
+# ---------------  shell context processor --------------- #
+# This is the shell context processor to make the database available in the shell
+# to run the shell, type: flask shell eg. >flask db init
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'Entry_Post': Entry_Post}
--- config.py
+++ config.py
@@ -3,9 +3,21 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    # Configure secret key
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # Configure database
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Configure Logging
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    #  email server 
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['me@summarizeme.io']
+    # Configure file upload
     UPLOAD_FOLDER = 'app/static/uploads'
     UPLOAD_CONTENT = 'app/text2summary/uploads'
     WRITE_JSON_LOCALLY = True
