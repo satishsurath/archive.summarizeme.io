67b1bc986fd39814ce1a7d7022eeae204b992572
Satish Surath <connect@sati.sh>
2023-04-12 04:33:37 -0400

    debug Interp
    Signed-off-by: Satish Surath <connect@sati.sh>

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,22 +1,27 @@
 import sys, os
 
 
-#If we are not in Production 
+#If we are not in Productionl
 # Or Development Mode
 # Or Staging Mode 
 # - then it loads the Python Path from the .env file
 
 if os.environ['HOME'] == '/home/dh_hjy3j9':
         INTERP = os.path.join(os.environ['HOME'], 'summarizeme.io', 'venv', 'bin', 'python3')
+        print("1:"+INTERP)
 elif os.environ['HOME'] == '/home/dh_wagsu9/':
         INTERP = os.path.join(os.environ['HOME'], 'dev.summarizeme.io', 'venv', 'bin', 'python3')
+        print("2:"+INTERP)
 elif os.environ['HOME'] == '/home/dh_kzhw5x':
         INTERP = os.path.join(os.environ['HOME'], 'beta.summarizeme.io', 'venv', 'bin', 'python3')
+        print("3:"+INTERP)
 else:   
-      from dotenv import load_dotenv
-      load_dotenv()
-      if (os.environ.get('INTERP')):
-        INTERP = os.environ.get('INTERP')
+        from dotenv import load_dotenv
+        load_dotenv()
+        if (os.environ.get('INTERP')):
+                INTERP = os.environ.get('INTERP')
+                print("4:"+INTERP)
+
 
 
 
