1130c21e48cd9fe36e88cb6e692a808b0754a324
Satish Surath <connect@sati.sh>
2023-04-12 04:06:04 -0400

    Refactoring selection of INTERP variable
    Signed-off-by: Satish Surath <connect@sati.sh>

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,16 +1,23 @@
 import sys, os
-INTERP = os.path.join(os.environ['HOME'], 'summarizeme.io', 'venv', 'bin', 'python3')
+
 
 #If we are not in Production 
 # Or Development Mode
 # Or Staging Mode 
 # - then it loads the Python Path from the .env file
 
-if os.environ['HOME'] == '/home/dh_hjy3j9' or os.environ['HOME'] == '/home/dh_wagsu9/' or os.environ['HOME'] == '/home/dh_kzhw5x':
-    from dotenv import load_dotenv
-    load_dotenv()
-    if (os.environ.get('INTERP')):
+if os.environ['HOME'] == '/home/dh_hjy3j9':
+        INTERP = os.path.join(os.environ['HOME'], 'summarizeme.io', 'venv', 'bin', 'python3')
+elif os.environ['HOME'] == '/home/dh_wagsu9/':
+        INTERP = os.path.join(os.environ['HOME'], 'dev.summarizeme.io', 'venv', 'bin', 'python3')
+elif os.environ['HOME'] == '/home/dh_kzhw5x':
+        INTERP = os.path.join(os.environ['HOME'], 'beta.summarizeme.io', 'venv', 'bin', 'python3')
+else:   
+      from dotenv import load_dotenv
+      load_dotenv()
+      if (os.environ.get('INTERP')):
         INTERP = os.environ.get('INTERP')
+
 
 
 if sys.executable != INTERP:
