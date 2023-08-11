23cafec0c837b241c573d3c5860a63f2b85fc4c9
Satish Surath <connect@sati.sh>
2023-08-11 11:23:57 -0400

    Adding Basic 'Copy to Clipboard' functionality
    Signed-off-by: Satish Surath <connect@sati.sh>

--- app/__init__.py
+++ app/__init__.py
@@ -68,7 +68,7 @@ if not app.debug:
             secure = ()
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            fromaddr='ErrorLogFile@' + app.config['MAIL_SERVER'],
             toaddrs=app.config['ADMINS'], subject='SummarizeMe.io Failure',
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
